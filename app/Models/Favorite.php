fa3ee7ff6d0f6c852cecb7e1531e31b16fea27f1
ryo_numoto <info@craftteck.com>
2024-12-13 15:18:13 +0900

    refactor: phpstanの警告を修正

--- app/Models/Favorite.php
+++ app/Models/Favorite.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Database\Factories\FavoriteFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Favorite extends Model
 {
+    /** @use HasFactory<FavoriteFactory> */
     use HasFactory;
 
     protected $guarded = ['id'];
